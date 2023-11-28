3db7dc41a54b13e41a59d64f954bd5d3b16e33e7
tatsianask108 <tt.laurie@gmail.com>
2023-11-28 20:04:42 +0300

    feat: implement switchTags function on menu page

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -7,15 +7,30 @@ function burgerFunction() {
         nav.classList.toggle("nav--visible");
         burgerBtn.classList.toggle("burger--active");
 
-
         navItems.forEach(el => {
             el.addEventListener('click', () => {
                 burgerBtn?.classList.remove('burger--active');
                 nav?.classList.remove('nav--visible');
             });
         });
-
     })
 }
 
 burgerFunction();
+
+
+const switchTags = () => {
+    const tagsContainer = document.querySelector('[data-tags]');
+    const tags = tagsContainer.querySelectorAll('div')
+    tagsContainer.addEventListener('click', (e) => {
+        if (e.target.closest("div")) {
+            const clickedTag = e.target.closest("div");
+            tags.forEach(tag => {
+                tag.classList.remove('tag__selected');
+            })
+            clickedTag.classList.add('tag__selected');
+        }
+    })
+}
+
+switchTags();
