5ec6f33d07c08a90cda60a4054af0abd2990f759
tatsianask108 <tt.laurie@gmail.com>
2023-11-28 13:29:35 +0300

    feat: implenet burger menu on menu page

--- coffee-house/js/main.js
+++ coffee-house/js/main.js
@@ -1,16 +1,21 @@
-const burgerBtn = document.getElementById("burger-btn");
-const nav = document.getElementById("nav");
-const navItems = nav?.querySelectorAll("a");
-const body = document.body;
+function burgerFunction() {
+    const burgerBtn = document.getElementById("burger-btn");
+    const nav = document.getElementById("nav");
+    const navItems = nav?.querySelectorAll("a");
 
-burgerBtn.addEventListener("click", () => {
-    nav.classList.toggle("nav--active");
-    burgerBtn.classList.toggle("burger--active");
+    burgerBtn.addEventListener("click", () => {
+        nav.classList.toggle("nav--active");
+        burgerBtn.classList.toggle("burger--active");
 
-    navItems.forEach(el => {
-        el.addEventListener('click', () => {
-            burgerBtn?.classList.remove('burger--active');
-            nav?.classList.remove('nav--active');
+
+        navItems.forEach(el => {
+            el.addEventListener('click', () => {
+                burgerBtn?.classList.remove('burger--active');
+                nav?.classList.remove('nav--active');
+            });
         });
-    });
-})
+
+    })
+}
+
+burgerFunction();
