7574c2be3aac4c033896e47ec630be0af56f363e
tatsianask108 <tt.laurie@gmail.com>
2023-12-05 18:05:13 +0300

    feat: implemet a pause when hovering the mouse on the element

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -1,34 +1,93 @@
 let sliderLine = document.querySelector('.slider-line'),
+    sliderContent = document.querySelectorAll('.slider__content'),
     prevButton = document.getElementById('btn-left'),
     nextButton = document.getElementById('btn-right'),
 
     sliderItems = sliderLine.querySelectorAll('img'),
     slidesCount = sliderItems.length,
 
-    currentSlide = 0;
+    controlsContainer = document.querySelector('.favorite-coffee__controls'),
+
+    currentSlide = 0,
+    isPaused = false;
 
 
-// let timerId = setInterval(() => setActiveSlide, 2000);
+//automatic change
+let timerId;
+
+
 function initSlider() {
     // init actions
-    nextButton.addEventListener('click', nextSlide)
-    prevButton.addEventListener('click', prevSlide)
+    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', prevSlide);
+
+
+
 
     setActiveSlide(0);
 }
 
 function nextSlide() {
-    setActiveSlide(currentSlide === slidesCount - 1 ? currentSlide : currentSlide + 1);
+    clearInterval(timerId);
+    setActiveSlide(currentSlide === slidesCount - 1 ? 0 : currentSlide + 1);
+    // isPaused = false;
+
 }
 function prevSlide() {
-    setActiveSlide(currentSlide === 0 ? 0 : currentSlide - 1);
+    clearInterval(timerId);
+    setActiveSlide(currentSlide === 0 ? slidesCount - 1 : currentSlide - 1);
+    // isPaused = false;
 }
 
 function setActiveSlide(index = 0) {
     index = +index;
 
-    let position = 480 * index;
-    sliderLine.style.left = -position + 'px';
+    let position = 100 * index;
+    sliderLine.style.left = -position + '%';
     currentSlide = index;
+
+    timerId = setInterval(() => {
+        if (!isPaused) {
+            nextSlide();
+        }
+    }, 3000);
+
+    // console.log(timerId)
+
+    for (let [key, control] of Object.entries(controlsContainer.children)) {
+
+        if (key == index) {
+            control.classList.add('control-active')
+        } else {
+            control.classList.remove('control-active');
+        }
+    }
+    pauseInterval()
+
 }
+
+function pauseInterval() {
+
+    sliderContent.forEach((item) => item.onmouseenter = () => {
+        isPaused = true;
+        console.log('pause')
+    })
+
+    sliderContent.forEach((item) => item.onmouseleave = () => {
+        isPaused = false;
+        console.log('play')
+    })
+
+    // sliderContent.forEach((item) => item.addEventListener('touchstart', () => {
+    //     isPaused = true;
+    //     console.log('touchstart')
+    // }))
+    // sliderContent.forEach((item) => item.addEventListener('touchend', () => {
+    //     isPaused = false;
+    //     console.log('touchend')
+    // }))
+
+}
+
+
 initSlider()
