e33e4722761b198fba36955acdae7e534d90fc81
tatsianask108 <tt.laurie@gmail.com>
2024-01-10 20:55:09 +0300

    feat: implement dynamic keyboard generation, game initialization

--- hangman/app.js
+++ hangman/app.js
@@ -1,16 +1,77 @@
+import { hints } from "./hints.js";
+
 const engKeyboard = [
   113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 97, 115, 100, 102, 103, 104, 106, 107, 108, 122,
   120, 99, 118, 98, 110, 109,
 ];
+
+let currentWord = word;
+let mistakesCount = 0;
+
+const keyboard = document.getElementById("keyboard");
+const hiddenWord = document.getElementById("word");
+
 const initKeyboard = () => {
-  let keyboard = "";
   for (let i = 0; i < engKeyboard.length; i++) {
+    const button = document.createElement("button");
+    button.classList.add("keyboard__btn");
+    button.innerText = String.fromCharCode(engKeyboard[i]);
     if (i === 10 || i === 19) {
-      keyboard += '<div class="new-line"></div>';
+      const newLiner = document.createElement("div");
+      newLiner.classList.add("new-line");
+      keyboard.append(newLiner);
     }
-    keyboard += `<div class="keyboard__btn">${String.fromCharCode(engKeyboard[i])}</div>`;
+    keyboard.append(button);
   }
-  document.getElementById("keyboard").innerHTML = keyboard;
 };
 
 initKeyboard();
+
+const pickRandomWord = () => {
+  const { word, hint } = hints[Math.round(Math.random() * hints.length)];
+  currentWord = word;
+  console.log(word);
+  document.getElementById("hint").innerText = hint;
+  document.getElementById("guesses-counter").innerText = `0 / ${word.length}`;
+
+  for (let i = 0; i < word.length; i++) {
+    const character = document.createElement("li");
+    character.classList.add("character");
+    hiddenWord.append(character);
+  }
+};
+
+pickRandomWord();
+
+const initGame = (button, character) => {
+  button.classList.add("disabled");
+  if (currentWord.includes(character)) {
+    [...currentWord].forEach((letter, index) => {
+      if (letter === character) {
+        hiddenWord.children[index].innerText = letter;
+        hiddenWord.children[index].classList.add("character--guessed");
+      }
+    });
+  } else {
+    document.getElementById("guesses-counter").innerText = `${mistakesCount + 1} / ${
+      currentWord.length
+    }`;
+    mistakesCount++;
+    if (mistakesCount === currentWord.length) {
+      showFailModal();
+    }
+  }
+};
+
+const addListeners = () => {
+  keyboard.addEventListener("click", (e) => {
+    initGame(e.target, e.target.innerText.toLowerCase());
+  });
+};
+
+addListeners();
+
+const showFailModal = () => {
+  const overlay = document.querySelector(".overlay").classList.remove("hidden");
+  overlay.querySelector;
+};
