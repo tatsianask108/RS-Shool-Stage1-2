01fd950576271825fe6ae80fdc7eb5fdfaa873cc
tatsianask108 <tt.laurie@gmail.com>
2024-02-20 17:43:59 +0300

    feat: migrate appView.js to TS

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { INewsResponse, IResponse } from '../../types/index';
 
 export class AppView {
+    private news; //?
+    private sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: INewsResponse): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: IResponse): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
