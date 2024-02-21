56eeac93b65fb2f7ef0c4da5f41824a282282ab7
tatsianask108 <tt.laurie@gmail.com>
2024-02-21 18:22:42 +0300

    refactor: implement type checking using predicates

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,22 +1,28 @@
 import './sources.css';
 import { ISource } from '../../../types/index';
+import { isDocumentFragment, isHTMLElement } from '../../../helpers/helpers';
 
 class Sources {
     draw(data: ISource[]): void {
         const fragment: DocumentFragment = document.createDocumentFragment();
         const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
-
+        if (!sourceItemTemp) {
+            return;
+        }
         data.forEach((item: ISource) => {
-            if (sourceItemTemp) {
-                const sourceClone: DocumentFragment = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
-                const itemNameElement: Element | null = sourceClone.querySelector('.source__item-name');
-                const itemElement: Element | null = sourceClone.querySelector('.source__item');
-                if (itemNameElement instanceof HTMLElement && itemElement instanceof HTMLElement) {
-                    itemNameElement.textContent = item.name;
-                    itemElement.setAttribute('data-source-id', item.id.toString());
-                }
-                fragment.append(sourceClone);
+            const sourceClone = sourceItemTemp.content.cloneNode(true);
+            if (!isDocumentFragment(sourceClone)) {
+                return;
             }
+            const sourceItemName = sourceClone.querySelector('.source__item-name');
+            const sourceItem = sourceClone.querySelector('.source__item');
+            if (!(isHTMLElement(sourceItemName) && isHTMLElement(sourceItem))) {
+                return;
+            }
+            sourceItemName.textContent = item.name;
+            sourceItem.setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
         });
         document.querySelector('.sources')?.append(fragment);
     }
