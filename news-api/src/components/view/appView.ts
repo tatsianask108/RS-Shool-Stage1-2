4d61e5c4b8c6a58f2a805153f167e715ce1aa3a6
tatsianask108 <tt.laurie@gmail.com>
2024-02-23 14:17:39 +0300

    feat: add Utility Type 'Readonly'

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -11,12 +11,12 @@ export class AppView {
         this.sources = new Sources();
     }
 
-    public drawNews(data: INewsResponse): void {
+    public drawNews(data: Readonly<INewsResponse>): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    public drawSources(data: IResponse): void {
+    public drawSources(data: Readonly<IResponse>): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
