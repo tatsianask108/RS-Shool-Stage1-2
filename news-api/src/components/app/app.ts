d59a45e670531370bf0f1db74849464befbcdada
tatsianask108 <tt.laurie@gmail.com>
2024-02-26 15:28:15 +0300

    feat: add pageUp functionality

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,5 +1,6 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { isHTMLElement } from '../../helpers/helpers';
 
 class App {
     private controller;
@@ -24,6 +25,30 @@ class App {
                 this.view.drawSources(data);
             }
         });
+        this.handleScroll();
+    }
+
+    private handleScroll(): void {
+        const toTopBtn = document.getElementById('toTopBtn');
+
+        window.onscroll = (): void => {
+            changeBtnVisibility();
+        };
+
+        function changeBtnVisibility(): void {
+            if (!isHTMLElement(toTopBtn)) {
+                throw new Error('top button does not exist in html template');
+            }
+            document.documentElement.scrollTop > 100
+                ? (toTopBtn.style.display = 'block')
+                : (toTopBtn.style.display = 'none');
+        }
+
+        document.getElementById('toTopBtn')?.addEventListener('click', () => pageUp());
+
+        function pageUp(): void {
+            document.documentElement.scrollTop = 0;
+        }
     }
 }
 
